610d16f667d58e4a3b42a5c39f9fddcd2dcb6de1
harrison4 <harrison@arthur.ai>
2021-09-29 22:10:50 -0400

    updated env vars

--- cmd/config.go
+++ cmd/config.go
@@ -1,12 +1,7 @@
 package main
 
 type EnvConfig struct {
-	PostgresUser     string `default:"user" split_words:"true"`
-	PostgresPassword string `split_words:"true"`
-	PostgresHost     string `default:"localhost" split_words:"true"`
-	PostgresPort     int    `default:"5432" split_words:"true"`
-	PostgresDb       string `default:"organization_service" split_words:"true"`
 	MigrationsPath   string `default:"file://pkg/database/migrations" split_words:"true"`
-	DatabaseURL      string `default:"" split_words:"true"`
+	DatabaseURL      string `default:"postgres://user:Password123!@localhost:5432/organization_service?sslmode=disable" split_words:"true"`
 	Port             int    `default:"8082" split_words:"true"`	// env var used by heroku to assign the port for the deployed app
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/kelseyhightower/envconfig"
 	log "github.com/sirupsen/logrus"
 	"organization_manager/pkg/api"
@@ -15,21 +14,13 @@ func main() {
 		log.Fatalf("error loading environment variables: %v", err.Error())
 	}
 
-	var dbURI string
-	if envConfig.DatabaseURL == "" {
-		dbURI = fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
-			envConfig.PostgresUser, envConfig.PostgresPassword, envConfig.PostgresHost, envConfig.PostgresPort,
-			envConfig.PostgresDb)
-	} else {
-		dbURI = envConfig.DatabaseURL
-	}
-	err = database.Initialize(dbURI)
+	err = database.Initialize(envConfig.DatabaseURL)
 	if err != nil {
 		log.Fatalf("error initializing the database: %v", err.Error())
 	}
 
 	// running migrations on startup
-	err = database.RunMigrations(envConfig.MigrationsPath, dbURI)
+	err = database.RunMigrations(envConfig.MigrationsPath, envConfig.DatabaseURL)
 	if err != nil {
 		log.Fatalf("error running database migration on startup: %v", err.Error())
 	}
