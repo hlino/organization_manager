8cc69c09a7e164c2e51f0623574970243d9b7711
harrison4 <harrison@arthur.ai>
2021-09-30 20:43:05 -0400

    added test for querying organizations

--- pkg/api/controllers/organizations_test.go
+++ pkg/api/controllers/organizations_test.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"bytes"
+	"database/sql/driver"
 	"encoding/json"
 	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
@@ -9,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"organization_manager/pkg/api/services"
 	"organization_manager/pkg/database"
 	"organization_manager/pkg/database/models"
 	"regexp"
@@ -40,13 +42,13 @@ func TestSaveNewOrganization(t *testing.T) {
 			requestBody: []byte(`{"invalid":"invalid",creation_date": "2021-09-26T00:00:00Z",
 								"employee_count": 10,"is_public": false}`),
 			expectedOrganization: emptyOrg,
-			expectedRespCode: http.StatusBadRequest,
+			expectedRespCode:     http.StatusBadRequest,
 		},
 		{
 			requestBody: []byte(`{"id": "1eacb0fa-d4ae-4d5e-9b69-268c1359db19", "name": "Organization 1","creation_date": "2021-09-26T00:00:00Z",
 								"employee_count": 10,"is_public": false}`),
 			expectedOrganization: emptyOrg,
-			expectedRespCode: http.StatusBadRequest,
+			expectedRespCode:     http.StatusBadRequest,
 		},
 	}
 
@@ -77,7 +79,7 @@ func TestSaveNewOrganization(t *testing.T) {
 				err := json.NewDecoder(res.Body).Decode(&respObj)
 				assert.NoError(t, err)
 				assert.NotEqual(t, uuid.Nil, respObj.ID)
-			} else if test.expectedRespCode ==http.StatusBadRequest {
+			} else if test.expectedRespCode == http.StatusBadRequest {
 				var respObj ErrorResponse
 				err := json.NewDecoder(res.Body).Decode(&respObj)
 				assert.NoError(t, err)
@@ -88,20 +90,80 @@ func TestSaveNewOrganization(t *testing.T) {
 }
 
 func TestGetOrganizations(t *testing.T) {
-	//req := httptest.NewRequest(http.MethodPost, "/organizations", bytes.NewBuffer(test.requestBody))
-	//w := httptest.NewRecorder()
-	//q := req.URL.Query()
-	//if test.userId != nil {
-	//	q.Add("user_id", (*test.userId).String())
-	//}
-	//if test.modelId != nil {
-	//	q.Add("model_id", (*test.modelId).String())
-	//}
-	//if test.page != nil {
-	//	q.Add("page", strconv.Itoa(*test.page))
-	//}
-	//if test.pageSize != nil {
-	//	q.Add("page_size", strconv.Itoa(*test.pageSize))
-	//}
-	//req.URL.RawQuery = q.Encode()
+	var organizationColumns = []string{"id", "name", "created_date", "employee_count", "is_public"}
+	var countCol = []string{"count"}
+	var tests = []struct {
+		queryParams              map[string][]string
+		expectedQueryConditional string
+		expectedQueryLimit       string
+		expectedArgs             []driver.Value
+		expectedResponseCode     int
+	}{
+		{
+			queryParams:              map[string][]string{"filter": {"name:CLEAR"}},
+			expectedQueryConditional: `WHERE name = $1`,
+			expectedQueryLimit:       `LIMIT 20`,
+			expectedArgs:             []driver.Value{"CLEAR"},
+			expectedResponseCode:     http.StatusOK,
+		},
+		{
+			queryParams:              map[string][]string{"filter": {"name:CLEAR"}, "range_filter": {"creation_date:[2002-09-22T00:00:00ZTO*]"}, "page": {"2"}},
+			expectedQueryConditional: `WHERE name = $1 AND creation_date >= $2`,
+			expectedQueryLimit:       `LIMIT 20 OFFSET 20`,
+			expectedArgs:             []driver.Value{"CLEAR", "2002-09-22T00:00:00Z"},
+			expectedResponseCode:     http.StatusOK,
+		},
+		{
+			queryParams:              map[string][]string{"filter": {"name:CLEAR"}, "range_filter": {"creation_date:[2002-09-22T00:00:00ZTO*]"}, "page": {"r"}},
+			expectedQueryConditional: `WHERE name = $1 AND creation_date >= $2`,
+			expectedQueryLimit:       `LIMIT 20 OFFSET 20`,
+			expectedArgs:             []driver.Value{"CLEAR", "2002-09-22T00:00:00Z"},
+			expectedResponseCode:     http.StatusInternalServerError,
+		},
+	}
+
+	_, mock, err := database.InitializeTest()
+	assert.NoError(t, err)
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test_%d", i), func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/organizations", nil)
+			w := httptest.NewRecorder()
+			q := req.URL.Query()
+
+			for queryKey, queryValue := range test.queryParams {
+				for _, value := range queryValue {
+					q.Add(queryKey, value)
+				}
+			}
+			req.URL.RawQuery = q.Encode()
+
+			mock.ExpectQuery(regexp.QuoteMeta(`SELECT count(*) FROM "organizations" ` + test.expectedQueryConditional)).
+				WithArgs(test.expectedArgs...).WillReturnRows(sqlmock.NewRows(countCol).AddRow(1))
+
+			mock.ExpectQuery(regexp.QuoteMeta(
+				`SELECT * FROM "organizations" ` + test.expectedQueryConditional + ` ORDER BY id ` + test.expectedQueryLimit)).
+				WithArgs(test.expectedArgs...).
+				WillReturnRows(sqlmock.NewRows(organizationColumns).
+					AddRow(uuid.New(), "CLEAR", "2002-09-22T00:00:00Z", 10000, true))
+
+			GetOrganizations(w, req)
+			res := w.Result()
+			assert.Equal(t, test.expectedResponseCode, res.StatusCode)
+
+			if test.expectedResponseCode == http.StatusOK {
+				assert.NoError(t, mock.ExpectationsWereMet())
+				var respObj services.PaginatedOrganizationResponse
+				err := json.NewDecoder(res.Body).Decode(&respObj)
+				assert.NoError(t, err)
+				// mock db call will always return a single org
+				assert.Equal(t, 1, len(respObj.Organizations))
+			} else {
+				var respObj ErrorResponse
+				err := json.NewDecoder(res.Body).Decode(&respObj)
+				assert.NoError(t, err)
+				assert.NotEqual(t, "", respObj.Error)
+			}
+		})
+	}
 }
